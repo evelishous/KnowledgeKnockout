491d88284f8ad315b40707d272f474795c954de2
Mats Thieme <mats.thieme.mt@gmail.com>
2019-12-11 15:43:32 +0100

    bla

--- KnowledgeKnockout/routes/index_route.ts
+++ KnowledgeKnockout/routes/index_route.ts
@@ -3,4 +3,5 @@ import { render } from "../views/render";
 
 export async function index_route_get(req: Request, res: Response) {
     res.send(await render(['index'], { title: 'welcome' }));
+    console.log('bla');
 }
