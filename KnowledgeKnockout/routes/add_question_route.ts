b3eb4ea8ddda091bbf0dd4dd493c1efd315443e8
Max Burow <max.burow@outlook.com>
2019-12-18 13:16:29 +0100

    update on KnowledgeKnockout/routes/add_question_route.ts

--- KnowledgeKnockout/routes/add_question_route.ts
+++ KnowledgeKnockout/routes/add_question_route.ts
@@ -5,26 +5,12 @@ import * as path from 'path';
 
 export function add_question_route_post(req: Request, res: Response) {
 
-	console.log(req.body);
+	MySQL.query('insert into question (content, blockId, topicId) values (?, ?, ?)', [req.body.content, req.body.topicBlockId, req.body.topicId]).then(
+		(results) => {
+		let lastQuestionId = results.insertId;
 
-	let questionContent: string = req.body.content;
-	let wrongAnswers: string[] = [
-		req.body.wrongAnswer01,
-		req.body.wrongAnswer02,
-		req.body.wrongAnswer03
-	];
-	let correctAnswer:string = req.body.correctAnswer;
-	let topicId: string = req.body.topicId;
-	let topicBlockId: string = req.body.topicBlockId;
-	let lastQuestionId: number = 0;
+		let sql: string = `insert into answer (questionId, content, isCorrect) values (${lastQuestionId}, ${mysql.escape(req.body.wrongAnswer01)}, false), (${lastQuestionId}, ${mysql.escape(req.body.wrongAnswer02)}, false), (${lastQuestionId}, ${mysql.escape(req.body.wrongAnswer03)}, false), (${lastQuestionId}, ${mysql.escape(req.body.correctAnswer)}, true)`;
 
-	let sql: string = 'insert into question (content, blockId, topicId) values (?, ?, ?)';
-	let inserts: string[] = [questionContent, topicBlockId, topicId];
-
-	MySQL.query(sql, inserts).then((results) => {
-		lastQuestionId = results.insertId;
-
-		let sql: string = `insert into answer (questionId, content, isCorrect) values (${lastQuestionId}, ${mysql.escape(wrongAnswers[0])}, false), (${lastQuestionId}, ${mysql.escape(wrongAnswers[1])}, false), (${lastQuestionId}, ${mysql.escape(wrongAnswers[2])}, false), (${lastQuestionId}, ${mysql.escape(correctAnswer)}, true)`;
 		MySQL.query(sql, []).then((results) => {
 			res.send(JSON.stringify({success: true}));
 		}, (error) => {
@@ -36,9 +22,5 @@ export function add_question_route_post(req: Request, res: Response) {
 }
 
 export function add_question_route_get(req: Request, res: Response) {
-
-	console.log(path.resolve('./dist/views/add_question.html'));
-
 	res.sendFile(path.resolve('./dist/views/add_question.html'));
-	//res.send(req.body);
 }
