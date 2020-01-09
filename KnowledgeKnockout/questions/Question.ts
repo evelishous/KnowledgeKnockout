09b1a4c12e39ee12121911a1bc59f4c95a8c557a
Mats Thieme <mats.thieme.mt@gmail.com>
2020-01-09 09:43:16 +0100

    adfgdaf

--- KnowledgeKnockout/questions/Question.ts
+++ KnowledgeKnockout/questions/Question.ts
@@ -4,11 +4,13 @@ export class Question {
     public topicId: number;
     public content: string;
     public imgSrc: string;
-    public constructor(questionId: number = 0, blockId: number = 0, topicId: number = 0, content: string = '', imgSrc: string = '') {
+    public secondsToSolve: number;
+    public constructor(questionId: number = 0, blockId: number = 0, topicId: number = 0, content: string = '', imgSrc: string = '', secondsToSolve: number = 0) {
         this.id = questionId;
         this.blockId = blockId;
         this.topicId = topicId;
         this.content = content;
         this.imgSrc = imgSrc;
+        this.secondsToSolve = secondsToSolve;
     }
 }
