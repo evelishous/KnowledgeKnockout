58172b27e994f9ae33f6b7cdd2b0768c1c610026
Mats Thieme <mats.thieme.mt@gmail.com>
2020-01-13 09:37:07 +0100

    login.js

--- KnowledgeKnockout/public/js/login.js
+++ KnowledgeKnockout/public/js/login.js
@@ -1,7 +1,6 @@
 const name = document.getElementById('name');
 const password = document.getElementById('password');
 const submitBtn = document.getElementById('submit');
-const bcrypt = window.dcodeIO.bcrypt;
 
 submitBtn.onclick = async e => {
     e.preventDefault();
@@ -10,7 +9,7 @@ submitBtn.onclick = async e => {
 
     let data = {
         name: name.value,
-        password: bcrypt.hashSync(password.value)
+        password: password.value
     };
 
     // validate input
@@ -27,36 +26,18 @@ submitBtn.onclick = async e => {
     if (errorString.length !== 0) {
         return alert(errorString);
     }
-    try {
-        const res = await fetch('/login', {
-            method: 'POST',
-            headers: { 'Content-Type': 'application/json' },
-            body: JSON.stringify(data)
-        });
-
-        const hash = await res.json();
-        const success = await bcrypt.compare(password.value, hash.value);
-
-        if (success == true) {
-            data.action = 'confirm';
-            const confirmationResponse = await fetch('/login', {
+    else {
+        try {
+            const res = await fetch('/login', {
                 method: 'POST',
                 headers: { 'Content-Type': 'application/json' },
                 body: JSON.stringify(data)
             });
 
-            const confirmation = await confirmationResponse.json();
+            alert(await res.json());
 
-            if (confirmation == true) {
-                alert('Login successful.');
-            }
-
-        } else {
-            alert('Login failed.');
+        } catch (error) {
+            alert(`Ein Fehler ist aufgetreten: ${error}`);
         }
-            
-
-    } catch (error) {
-        alert(`Ein Fehler ist aufgetreten: ${error}`);
     }
 };
