1968d8342585f8d0b406741531594a5ca0d8d295
Mats Thieme <mats.thieme.mt@gmail.com>
2020-01-13 09:34:05 +0100

    removed login_encrypted_route and dependencies

--- KnowledgeKnockout/KnowledgeKnockout.ts
+++ KnowledgeKnockout/KnowledgeKnockout.ts
@@ -2,16 +2,16 @@ import * as compression from 'compression';
 import * as express from 'express';
 import * as session from 'express-session';
 import * as helmet from 'helmet';
+import { resolve } from 'path';
 import { add_question_route_get, add_question_route_post } from './routes/add_question_route';
 import { any_route_get } from './routes/any_route';
 import { index_route_get } from './routes/index_route';
-import { login_route_get, login_route_post, login_route_encrypted } from './routes/login_route';
+import { login_route_get, login_route_post } from './routes/login_route';
 import { logout_route_get } from './routes/logout_route';
 import { registration_route_get, registration_route_post } from './routes/registration_route';
 import { socketiotest_get_route } from './routes/socketiotest_get_route';
 import { training_route_get, training_route_post } from './routes/training_route';
 import { SocketConnection } from './socket_connection/SocketConnection';
-import { resolve } from 'path';
 
 const app = express();
 
@@ -57,7 +57,7 @@ app.get('/add-question', add_question_route_get).post('/add-question', add_quest
 
 app.get('/register', registration_route_get).post('/register', registration_route_post);
 
-app.get('/login', login_route_get).post('/login', login_route_encrypted);
+app.get('/login', login_route_get).post('/login', login_route_post);
 app.get('/logout', logout_route_get);
 
 app.get('/training', training_route_get).post('/training', training_route_post);
