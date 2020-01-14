759f5160a4c801699ef86bf33b8bb8105c83013a
Mats Thieme <mats.thieme.mt@gmail.com>
2020-01-14 11:31:49 +0100

    build script fix

--- KnowledgeKnockout/build.js
+++ KnowledgeKnockout/build.js
@@ -2,11 +2,14 @@ const fs = require('fs');
 const path = require('path');
 
 if (fs.existsSync('./dist/public')) {
-    fs.rmdirSync('./dist/public');
+    deleteFolderRecursive('./dist/public');
 }
 if (fs.existsSync('./dist/views')) {
-    fs.rmdirSync('./dist/views');
+    deleteFolderRecursive('./dist/views');
 }
+
+copyFolderSync('./public', './dist/public');
+copyFolderSync('./views', './dist/views');
 
 function copyFolderSync(from, to) { // https://stackoverflow.com/questions/13786160/copy-folder-recursively-in-node-js#answer-52338335
     fs.mkdirSync(to);
@@ -19,5 +22,17 @@ function copyFolderSync(from, to) { // https://stackoverflow.com/questions/13786
     });
 }
 
-copyFolderSync('./public', './dist/public');
-copyFolderSync('./views', './dist/views');
+
+function deleteFolderRecursive(path_) { // https://stackoverflow.com/questions/18052762/remove-directory-which-is-not-empty#answer-32197381
+    if (fs.existsSync(path_)) {
+        fs.readdirSync(path_).forEach((file, index) => {
+            const curPath = path.join(path_, file);
+            if (fs.lstatSync(curPath).isDirectory()) { // recurse
+                deleteFolderRecursive(curPath);
+            } else { // delete file
+                fs.unlinkSync(curPath);
+            }
+        });
+        fs.rmdirSync(path_);
+    }
+};
